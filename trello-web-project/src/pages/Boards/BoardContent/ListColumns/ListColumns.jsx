7d04118d4a1ef24f85ef83dc102d1af20fa03662
QUANG221222 <nguyennhatquang.2509@gmail.com>
2024-12-28 10:35:30 +0700

    Update Trello Board Content P2

---
+++ trello-web-project/src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -0,0 +1,50 @@
+import Button from "@mui/material/Button";
+import Column from "./Column/Column";
+import Box from "@mui/material/Box";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+
+function ListColumns() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        bgcolor: "inherit",
+        width: "100%",
+        height: "100%",
+        overflowX: "auto",
+        overflowY: "hidden",
+        "&::-webkit-scrollbar-track": { m: 2 },
+      }}
+    >
+      <Column />
+      <Column />
+      <Column />
+      {/* Box Add new column CTA */}
+      <Box
+        sx={{
+          minWidth: "200px",
+          maxWidth: "200px",
+          mx: 2,
+          borderRadius: "6px",
+          height: "fit-content",
+          bgcolor: "#ffffff3d",
+        }}
+      >
+        <Button
+          startIcon={<NoteAddIcon />}
+          sx={{
+            color: "white",
+            width: "100%",
+            justifyContent: "flex-start",
+            pl: 2.5,
+            py: 1,
+          }}
+        >
+          Add New Column
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default ListColumns;
